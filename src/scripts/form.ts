4d191fa07008418e0152f511d11c342ccda45d93
torturado <73028283+torturado@users.noreply.github.com>
2025-10-30 21:30:06 +0100

    refactor: update import paths to use alias and improve form handling by accessing elements by name

--- src/scripts/form.ts
+++ src/scripts/form.ts
@@ -2,7 +2,7 @@
 export function handleFormSubmit(event: Event): void {
   event.preventDefault();
   const form = event.target as HTMLFormElement;
-  const name = (form.name as unknown as HTMLInputElement).value;
-  const surname = (form.surname as unknown as HTMLInputElement).value;
+  const name = (form.elements.namedItem('name') as HTMLInputElement).value;
+  const surname = (form.elements.namedItem('surname') as HTMLInputElement).value;
   console.log(`Nom: ${name}\nCognoms: ${surname}`);
 }
